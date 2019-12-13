4fb16a50220644f8651464fea172f9df60e3a39a
JuanCarlosgg <juancarlos97gg@gmail.com>
2019-12-13 17:42:03 +0100

    AA

--- lib/reminders.py
+++ lib/reminders.py
@@ -192,10 +192,10 @@ class ReminderData:
         log("IDReminderData: JSONifying")
         rec_list = []
         for reminder in self._reminders:
-            ms = self._get_ms_time(reminder[0], reminder[1])
+            #ms = self._get_ms_time(reminder[0], reminder[1])
             concept = reminder[2]
             id = reminder[3]
-            r_dict = {'tiempo': ms, 'sonido': concept, 'id': id }
+            r_dict = {'tiempo': reminder[0], 'dia': reminder[1],  'sonido': concept, 'id': id }
             rec_list.append(r_dict)
         json_dict = {'recordatorios': rec_list}
         return json.dumps(json_dict)
